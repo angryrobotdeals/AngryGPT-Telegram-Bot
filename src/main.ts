c0e12858eeaab5e854389952aab97211f3b9f77b
angryrobotdeals <121421580+angryrobotdeals@users.noreply.github.com>
2023-05-12 14:55:59 +0300

    Initial commit #1.2

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import * as process from 'process';
 
 require('dotenv').config();
 
 async function bootstrap() {
-  const app = await NestFactory.createApplicationContext(AppModule);
+  await NestFactory.createApplicationContext(AppModule);
+  // const app = await NestFactory.createApplicationContext(AppModule);
   // await app.listen(3000);
 }
 
